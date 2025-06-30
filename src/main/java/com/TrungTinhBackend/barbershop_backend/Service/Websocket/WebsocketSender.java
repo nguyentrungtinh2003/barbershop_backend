1f4a54252cf1f386ef3c15a4716aba71994ee7d6
nguyentrungtinh <trungtinhn300@gmail.com>
2025-06-30 09:44:05 +0700

    Add websocket config basic

---
+++ src/main/java/com/TrungTinhBackend/barbershop_backend/Service/Websocket/WebsocketSender.java
@@ -0,0 +1,17 @@
+package com.TrungTinhBackend.barbershop_backend.Service.Websocket;
+
+import com.TrungTinhBackend.barbershop_backend.DTO.FeedbackDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebsocketSender {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public void sendFeedback(FeedbackDTO feedbackDTO) {
+        simpMessagingTemplate.convertAndSend("/topic/"+feedbackDTO.getId(),feedbackDTO);
+    }
+}
